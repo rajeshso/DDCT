e6c4f294ab14bd3d3b198f1b94a86d9c296f6171
Rajesh Somasundaram <fun_rajesh@yahoo.com>
2017-01-21 19:53:33 +0000

    Introduce Disable Service Charges by default and do not break existing tests

--- src/main/scala/gov/hmrc/ddct/cart/ShoppingCart.scala
+++ src/main/scala/gov/hmrc/ddct/cart/ShoppingCart.scala
@@ -1,8 +1,11 @@
 package gov.hmrc.ddct.cart
 
+import gov.hmrc.ddct.gov.hmrc.ddct.tip.{DisableServiceCharge, EnableServiceCharge, ServiceCharge}
 import gov.hmrc.ddct.menu._
 
-class ShoppingCart {
+import scala.math.BigDecimal.RoundingMode
+
+class ShoppingCart(additionalCharges: ServiceCharge = DisableServiceCharge) {
   private var items = List.empty[Item]
   private val prices: Map[Item, Double] = Map(Cola -> 0.5, Coffee -> 1, CheeseSandwich -> 2, SteakSandwich -> 4.5)
 
@@ -11,8 +14,16 @@ class ShoppingCart {
   def +=(item: Item): Unit = items = items :+ item
 
   def cartItemsTotalPrice: Double = {
-    items.foldLeft(0D) { (total, item) =>
-      total + prices(item)
+    def totalBilllWithoutServiceCharge: Double =
+      items.foldLeft(0D) { (total, item) =>
+        total + prices(item)
+      }
+
+    additionalCharges match {
+      case EnableServiceCharge =>
+        ???
+      case DisableServiceCharge => BigDecimal(totalBilllWithoutServiceCharge).setScale(2, RoundingMode.HALF_UP).toDouble
     }
   }
+
 }
---
+++ src/main/scala/gov/hmrc/ddct/gov/hmrc/ddct/tip/ServiceCharge.scala
@@ -0,0 +1,7 @@
+package gov.hmrc.ddct.gov.hmrc.ddct.tip
+
+trait ServiceCharge
+
+case object EnableServiceCharge extends ServiceCharge
+
+case object DisableServiceCharge extends ServiceCharge
